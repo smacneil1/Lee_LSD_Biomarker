58a023aab094b131878c61b2e7fd2d5c74e1bc90
smacneil1 <smacneil88@gmail.com>
2016-07-28 11:50:58 -0600

    added ssGSEA

---
+++ Rscripts/ssGSEA.R
@@ -0,0 +1,16 @@
+# ---
+# title: "ssGSEA"
+# author: "Shelley MacNeil"
+# date: "July 25, 2016"
+# purpose: run ssGSEA
+# ---
+
+#specify paths
+RNAseqData=
+
+#read in the files 
+
+
+f= as.matrix(read.table(, stringsAsFactors = FALSE, header=FALSE,sep='\t', quote="", row.names = 1))
+  
+  
